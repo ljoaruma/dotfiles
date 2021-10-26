a638dad04040bd35349d44872c6206de80c732a1
ljoaruma <ljoaruma@users.noreply.github.com>
2021-10-26 18:19:04 +0900

    add bashrc

--- bash/aliasrc.sh
+++ bash/aliasrc.sh
@@ -21,6 +21,8 @@ alias p9='pushd +9'
 
 alias pback='pushd "${OLDPWD}"'
 
+! readlink -f . &> /dev/null && which greadlink &> /dev/null && alias readlink='greadlink'
+
 alias rsyncn='rsync -avhsn --stats --progress'
 alias rsynca='rsync -avhs --stats --progress'
 alias rsynca_inlog='rsync -avhs --stats --progress --log-file-format="%i %o %l/%b %M %f %L"'
