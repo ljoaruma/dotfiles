c0150504bb8345a1cb6037c4121322333009101c
ljoaruma <ljoaruma@users.noreply.github.com>
2022-01-29 18:12:51 +0900

    change WSL environment check

--- bash/utils.sh
+++ bash/utils.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # wslの場合だけのユーティリティ関数追加
-if [[ -v WSL_DISTRO_NAME ]]; then
+#if [[ -v WSL_DISTRO_NAME ]]; then
+if uname --kernel-release | grep -q -e "WSL" &> /dev/null ; then
   source "$(dirname "${BASH_SOURCE}")"/wsl-utils.sh
 fi
 
