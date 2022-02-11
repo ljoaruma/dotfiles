9df5b88643541ccf607ba3d676e8f5616a5cf92f
ljoaruma <ljoaruma@users.noreply.github.com>
2022-02-11 19:40:51 +0900

    improved profile startup service, more mount check

--- profile/wsl-service.sh
+++ profile/wsl-service.sh
@@ -1,19 +1,65 @@
 #!/bin/bash
 
 # WSL2 service starting
+
+if [[ ! -v WSLENV ]]; then
+  return 1
+fi
+
+__restart_service () {
+  if [[ ! -x /etc/init.d/$1 ]]; then
+    return 1
+  fi
+
+  if service $1 status &> /dev/null; then
+    return 1
+  fi
+
+  sudo service $1 restart &> /dev/null
+
+  return 0
+}
+
+__restart_anacron () {
+  if [[ ! -x /etc/init.d/anacron ]]; then
+    return 1
+  fi
+
+  sudo service stop anacron
+  sudo service start anacron
+}
+
+mapfile _fstab_reserve < <(awk '$3~/\<drvfs\>/ { print $2 }' /etc/fstab)
+if [[ $(mount -t 9p | awk '{ print $3 }' | grep -F "$(IFS=''; echo "${_fstab_reserve[*]}")" | wc -l) -ne ${#_fstab_reserve[@]} ]]; then
+
+  unset _fstab_reserve
+  return 1
+fi
+
 if service ssh status &> /dev/null; then
   # SSHから、fstabで設定しているmountが見えていない場合は、restartする
   # WSL2の/initの関係でWindowsとの連携が上手くいってない可能性が高い
-  if ! ssh ryo@localhost mount | grep -q "\/mnt\/[wxy] " &> /dev/null; then
-    sudo service ssh restart &> /dev/null
-    sudo service cron restart &> /dev/null
+  if [[ $(ssh $USER@localhost mount -t 9p | awk '{ print $3 }' | grep -F "$(IFS=''; echo "${_fstab_reserve[*]}")" | wc -l) -ne ${#_fstab_reserve[@]} ]]; then
+
+    __restart_service rsyslog
+    __restart_service ssh
+    __restart_service cron
+
+    unset _fstab_reserve
+    unset -f __restart_service
+    unset -f __restart_anacron
+
+    return
   fi
-else
-  sudo service ssh restart &> /dev/null
-  sudo service cron restart &> /dev/null
 fi
 
-if ! service cron status &> /dev/null; then
-  sudo service cron restart &> /dev/null
-fi
+__restart_service rsyslog
+__restart_service ssh
+## cronを起動した時だけ、anacronも起動
+#__restart_service cron && __restart_anacron
+__restart_service cron
+
+unset _fstab_reserve
+unset -f __restart_service
+unset -f __restart_anacron
 
