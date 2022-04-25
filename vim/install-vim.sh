f197e376a60e736d79fd6a7acb4d8bf0887eeaef
ljoaruma <ljoaruma@users.noreply.github.com>
2022-04-25 21:27:13 +0900

    improved: vim setup

--- vim/install-vim.sh
+++ vim/install-vim.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 # vim: set ts=2 sw=2 et si filetype=bash :
 
-./configure \
-  --with-features=huge \
-  --prefix="$HOME/usr" \
-  --enable-python3interp \
-  --enable-rubyinterp \
-  --enable-cscope \
-  --enable-terminal \
-  --enable-multibyte \
-  --enable-gui=no \
-  --with-luajit \
-  --without-x \
-  --enable-fail-if-missing &&
+. "$(dirname "${BASH_SOURCE:-$0}")"/configure-vim.sh &&
 make clean && make && make install || { echo "failed vim imstall"; exit 1; }
 
