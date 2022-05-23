e03dcb5d3976dd9dfcf9e8bbcc9ed2ea39b85288
ljoaruma <ljoaruma@users.noreply.github.com>
2022-05-23 17:32:55 +0900

    fix: add luainterp option on vim configure

--- vim/configure-vim.sh
+++ vim/configure-vim.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash
 
+mkdir -p "$HOME/var/log"
+LOGFILE="$HOME/var/log/vim-configure-$(date "+%Y%m%d-%H%M%S")"
+touch "${LOGFILE}"
+
+declare -p PATH >> "${LOGFILE}"
+cat $0 >> "${LOGFILE}"
+echo "$*" >> "${LOGFILE}"
+
 ./configure \
   --with-features=huge \
   --prefix="$HOME/usr" \
   --enable-python3interp \
   --enable-rubyinterp \
+  --enable-luainterp \
   --enable-cscope \
   --enable-terminal \
   --enable-multibyte \
   --enable-gui=no \
   --with-luajit \
   --without-x \
-  --enable-fail-if-missing
+  --enable-fail-if-missing \
+  "$@" | tee -a "${LOGFILE}"
+
