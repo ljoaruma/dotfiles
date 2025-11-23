daf46f9481fe67a787a7c068001a26a552f1ba90
ljoaruma <9197675+ljoaruma@users.noreply.github.com>
2025-11-23 21:57:40 +0900

    profileのセットアップを改善
    - セットアップ済みかどうかのチェックを追加
    - 既存の.profileの先頭にカスタムprofileのsourceを追加

--- profile/setup/profile-setup.sh
+++ profile/setup/profile-setup.sh
@@ -16,32 +16,52 @@ mkdir -vp ${HOME}/.local/{src,bin,lib,include,sbin,share,opt}
 ## my .profile to conf directory
 readonly MY_DOT_PROFILE_DIRECTORY_="$(cd $(dirname ${BASH_SOURCE:-$0})/.. && pwd -P)"
 readonly MY_DOT_PROFILE_="${MY_DOT_PROFILE_DIRECTORY_}/config/profile"
-readonly MY_DOT_PROFILE_PLACED="${XDG_CONFIG_HOME:-${HOME}/.config}/profile"
-ln -sf "${MY_DOT_PROFILE_}" "${MY_DOT_PROFILE_PLACED}"
+readonly MY_DOT_PROFILE_PLACED_="${XDG_CONFIG_HOME:-${HOME}/.config}/profile"
+ln -sf "${MY_DOT_PROFILE_}" "${MY_DOT_PROFILE_PLACED_}"
 
 ## .profile add source .profile function 
 
 ### source .profile function
 
 #### my .profile path replace /home/username -> ${HOME}
-readonly MY_DOT_PROFILE_PATH="${MY_DOT_PROFILE_PLACED/#${HOME}/\$\{HOME\}}"
+readonly MY_DOT_PROFILE_PATH_="${MY_DOT_PROFILE_PLACED_/#${HOME}/\$\{HOME\}}"
+if grep -q -F -e "${MY_DOT_PROFILE_PATH_}" ~/.profile; then # grepオプション -F は固定文字列での検索を指定
+  # すでに.profileにカスタムprofileがある場合はセットアップ済みとして終了
+  echo ".profile already setup(added source function)"
+  exit 0
+fi
 #### source function
 readonly INSERT_PROFILE_COMMAND_="$(
 cat - <<EOL
 
 # source by my profile
-if [ -f "${MY_DOT_PROFILE_PATH}" ]; then
-  . "${MY_DOT_PROFILE_PATH}"
+if [ -f "${MY_DOT_PROFILE_PATH_}" ]; then
+  . "${MY_DOT_PROFILE_PATH_}"
 fi
+
+#
+
+
 EOL
 )"
 
 ### original .profile backup
 readonly STORED_CONF_DIRECTORY_="${XDG_DATA_HOME:-${HOME}/.local/share}/.storedconf/profile"
+if [ -f  "${STORED_CONF_DIRECTORY_}/.profile" ]; then
+  # すでに退避済みのファイルがある場合はセットアップ済みとして終了
+  echo ".profile already setup(stored)"
+  exit 0
+fi
 mkdir -vp "${STORED_CONF_DIRECTORY_}"
 cp --backup=t -vpL $HOME/.profile "${STORED_CONF_DIRECTORY_}"
 
 ### add function
-#echo "${INSERT_PROFILE_COMMAND_}"
-echo "${INSERT_PROFILE_COMMAND_}" >> $HOME/.profile
+### ---
+### 先頭にカスタム.profileのsourceを追加して既存の.profile, 最後に改行を追加
+### bashrc読み込み前にカスタム.profileを読むこむため先頭にsourceを追加する
+
+echo "${INSERT_PROFILE_COMMAND_}" > $HOME/.profile
+echo "" >> $HOME/.profile
+cat "${STORED_CONF_DIRECTORY_}/.profile" >> $HOME/.profile
+echo "" >> $HOME/.profile
 
