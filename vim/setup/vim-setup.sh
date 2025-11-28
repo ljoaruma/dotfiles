5b4448b0b82eb7228a09b233dbc8a2469741c28c
ljoaruma <9197675+ljoaruma@users.noreply.github.com>
2025-11-28 21:36:50 +0900

    vim setup script 作成

--- vim/setup/vim-setup.sh
+++ vim/setup/vim-setup.sh
@@ -1,20 +1,27 @@
 #!/usr/bin/env bash
 # vim: set ts=2 sw=2 et si filetype=bash :
 
-declare -r SOURCE_DIR="$(dirname $(readlink -f "$0"))"
+# エラー発生時は即中断
+set -eu -o pipefail
 
-mkdir -vp $HOME/usr/src
-cd $HOME/usr/src
-git clone https://github.com/vim/vim.git
+declare -r SOURCE_DIR_="$(dirname $(readlink -f "${BASH_SOURCE:-$0}"))"
 
-cd vim
-declare -r TARGET_TAG=$(git tag | tail -n1)
+# my vimruntime directory
+readonly DOTFILES_VIMCONF_DIRECTORY="$(cd $SOURCE_DIR_/../config && pwd -P)"
+readonly MY_VIMCONF_DIRECTORY="$(cd ${XDG_CONFIG_HOME:-${HOME}/.config} && pwd -P)/vim"
 
-git switch "${TARGET_TAG}" -c track/release
+if [ -d "${MY_VIMCONF_DIRECTORY}" ]; then
+  echo "exist ${MY_VIMCONF_DIRECTORY}(directory)"
+  return
+fi
+if [ -f "${MY_VIMCONF_DIRECTORY}" ]; then
+  echo "exist ${MY_VIMCONF_DIRECTORY}(file)"
+  return
+fi
+if [ -e "${MY_VIMCONF_DIRECTORY}" ]; then
+  echo "exist ${MY_VIMCONF_DIRECTORY}(other)"
+  return
+fi
 
-#. "${SOURCE_DIR}/vim/install-vim.sh"
-
-echo "OK, Next Step."
-echo "cd $(pwd)"
-echo "\"${SOURCE_DIR}/vim/install-vim.sh\""
+ln -sfv "${DOTFILES_VIMCONF_DIRECTORY}" "${MY_VIMCONF_DIRECTORY}"
 
