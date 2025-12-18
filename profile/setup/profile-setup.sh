ecc8e795d6bbe13951a2d5742447cc5becd48d4e
ljoaruma <9197675+ljoaruma@users.noreply.github.com>
2025-12-18 22:16:14 +0900

    improved profile structure

--- profile/setup/profile-setup.sh
+++ profile/setup/profile-setup.sh
@@ -4,13 +4,26 @@
 # エラー発生時は即中断
 set -eu -o pipefail
 
+# prepared
+# ===
+
+## my .profile to conf directory
+readonly MY_DOT_PROFILE_ROOT_DIRECTORY_="$(cd $(dirname ${BASH_SOURCE:-$0})/.. && pwd -P)"
+readonly MY_DOT_PROFILE_CONFIG_DIRECTORY_="${MY_DOT_PROFILE_ROOT_DIRECTORY_}/config"
+readonly MY_DOT_PROFILE_PATH_="${MY_DOT_PROFILE_CONFIG_DIRECTORY_}/profile"
+
+. "${MY_DOT_PROFILE_PATH_}"
+
+readonly PLACED_DOTFILE_DIRECTORY_="${XDG_CONFIG_HOME}/profile"
+readonly PLACED_DOTFILE_PATH_="${PLACED_DOTFILE_DIRECTORY_}/profile"
+
 # Create XDB Base Directory
 # ===
 
-mkdir -vp "${XDG_CONFIG_HOME:-${HOME}/.config}"
-mkdir -vp "${XDG_CACHE_HOME:-${HOME}/.cache}"
-mkdir -vp "${XDG_DATA_HOME:-${HOME}/.local/share}"
-mkdir -vp "${XDG_STATE_HOME:-${HOME}/.local/state}"
+mkdir -vp "${XDG_CONFIG_HOME}"
+mkdir -vp "${XDG_CACHE_HOME}"
+mkdir -vp "${XDG_DATA_HOME}"
+mkdir -vp "${XDG_STATE_HOME}"
 
 mkdir -vp ${HOME}/.local/{src,bin,lib,include,sbin,share,opt}
 #mkdir -vp ${HOME}/.local/{src,bin,lib,include,sbin,share,opt}
@@ -20,57 +33,76 @@ mkdir -vp ${HOME}/.local/{src,bin,lib,include,sbin,share,opt}
 # setup profile source 
 # ===
 
-## my .profile to conf directory
-readonly MY_DOT_PROFILE_DIRECTORY_="$(cd $(dirname ${BASH_SOURCE:-$0})/.. && pwd -P)"
-readonly MY_DOT_PROFILE_="${MY_DOT_PROFILE_DIRECTORY_}/config/profile"
-readonly MY_DOT_PROFILE_PLACED_="${XDG_CONFIG_HOME:-${HOME}/.config}/profile"
-ln -sf "${MY_DOT_PROFILE_}" "${MY_DOT_PROFILE_PLACED_}"
+if [ -L "${PLACED_DOTFILE_DIRECTORY_}" ]; then
+  unlink "${PLACED_DOTFILE_DIRECTORY_}"
+fi
+if [ -e "${PLACED_DOTFILE_DIRECTORY_}" ]; then
+  echo "exist ${PLACED_DOTFILE_DIRECTORY_}"
+  exit 0
+fi
+
+ln -s "${MY_DOT_PROFILE_CONFIG_DIRECTORY_}" "${PLACED_DOTFILE_DIRECTORY_}"
 
 ## .profile add source .profile function 
 
 ### source .profile function
 
 #### my .profile path replace /home/username -> ${HOME}
-readonly MY_DOT_PROFILE_PATH_="${MY_DOT_PROFILE_PLACED_/#${HOME}/\$\{HOME\}}"
-if grep -q -F -e "${MY_DOT_PROFILE_PATH_}" ~/.profile; then # grepオプション -F は固定文字列での検索を指定
+readonly PLACED_DOTFILE_PATH_UNEXPAND_="${PLACED_DOTFILE_PATH_/#${HOME}/\$\{HOME\}}"
+if grep -q -F -e "${PLACED_DOTFILE_PATH_UNEXPAND_}" ~/.profile; then # grepオプション -F は固定文字列での検索を指定
   # すでに.profileにカスタムprofileがある場合はセットアップ済みとして終了
   echo ".profile already setup(added source function)"
   exit 0
 fi
+
+#### original .profile backup
+readonly PLACED_DEFAULT_DOT_PROFILE_="${PLACED_DOTFILE_DIRECTORY_}/profile.default"
+# default profile path replace /home/username -> ${HOME}
+readonly PLACED_DEFAULT_DOT_PROFILE_UNEXPAND_="${PLACED_DEFAULT_DOT_PROFILE_/#${HOME}/\$\{HOME\}}"
+
+if [ -f $HOME/.profile ]; then
+
+  readonly STORED_CONF_DIRECTORY_="${XDG_DATA_HOME}/dotfiles/stored/profile"
+  if [ -f  "${STORED_CONF_DIRECTORY_}/.profile" ]; then
+    # すでに退避済みのファイルがある場合はセットアップ済みとして終了
+    echo ".profile already setup(stored)"
+    exit 0
+  fi
+
+  mkdir -vp "${STORED_CONF_DIRECTORY_}"
+  cp --backup=t -vpL $HOME/.profile "${STORED_CONF_DIRECTORY_}"
+  mv -i $HOME/.profile "${PLACED_DEFAULT_DOT_PROFILE_}"
+
+fi
+
 #### source function
+#### ---
+#### 先頭にカスタム.profileのsource、既存の.profile, 最後に改行を追加
+#### bashrc読み込み前にカスタム.profileを読むこむため先頭にsourceを追加する
+
 readonly INSERT_PROFILE_COMMAND_="$(
 cat - <<EOL
 
 # source by my profile
-if [ -f "${MY_DOT_PROFILE_PATH_}" ]; then
-  . "${MY_DOT_PROFILE_PATH_}"
+if [ -f "${PLACED_DOTFILE_PATH_UNEXPAND_}" ]; then
+  . "${PLACED_DOTFILE_PATH_UNEXPAND_}"
 fi
 
-#
+# source by default profile
+if [ -f "${PLACED_DEFAULT_DOT_PROFILE_UNEXPAND_}" ]; then
+  . "${PLACED_DEFAULT_DOT_PROFILE_UNEXPAND_}"
+fi
 
 
 EOL
 )"
 
-### original .profile backup
-readonly STORED_CONF_DIRECTORY_="${XDG_DATA_HOME:-${HOME}/.local/share}/dotfiles/stored"
-if [ -f  "${STORED_CONF_DIRECTORY_}/.profile" ]; then
-  # すでに退避済みのファイルがある場合はセットアップ済みとして終了
-  echo ".profile already setup(stored)"
-  exit 0
-fi
-mkdir -vp "${STORED_CONF_DIRECTORY_}"
-cp --backup=t -vpL $HOME/.profile "${STORED_CONF_DIRECTORY_}"
-
 ### add function
 ### ---
-### 先頭にカスタム.profileのsourceを追加して既存の.profile, 最後に改行を追加
-### bashrc読み込み前にカスタム.profileを読むこむため先頭にsourceを追加する
 
-echo "${INSERT_PROFILE_COMMAND_}" > $HOME/.profile
-echo "" >> $HOME/.profile
-cat "${STORED_CONF_DIRECTORY_}/.profile" >> $HOME/.profile
-echo "" >> $HOME/.profile
+#echo "${INSERT_PROFILE_COMMAND_}" > $HOME/.profile
+rm -f $HOME/.profile
+ln -s "${MY_DOT_PROFILE_CONFIG_DIRECTORY_}/root_profile" $HOME/.profile
 
 ###
 
