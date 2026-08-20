c2d6b06703d215bb5ba7048f2c259a1890f1fa66
ljoaruma <9197675+ljoaruma@users.noreply.github.com>
2026-08-20 22:43:33 +0900

    rust設定追加

---
+++ rust/setup/rust-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# https://doc.rust-lang.org/cargo/getting-started/installation.html
+
+export RUSTUP_HOME=$HOME/.local/opt/rustup
+export CARGO_HOME=$HOME/.local/opt/cargo
+
+curl https://sh.rustup.rs -sSf | sh
+
+
